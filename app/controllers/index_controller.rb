28320fddfdd0c8d49253f0c7b91ae608d52d53ee
Chris Chapman <chapmanchri@gmail.com>
2015-09-19 09:21:11 -0400

    don't allow words with duplicate letters

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -10,7 +10,7 @@ end
 
 post '/' do
   @word = params[:word]
-  if three_letters?(@word)
+  if valid_input?(@word)
     redirect "/anagrams/#{@word}"
   else
     erb :index
@@ -18,12 +18,27 @@ post '/' do
 end
 
 def three_letters?(input)
-  puts "#{input.length} ***********************************"
   if input.length <= 3 and input.length >= 1
-    puts "************************* input length ok"
     return true
   else
-    puts "************************* input length bad"
     return false
+  end
+end
+
+def distinct_letters?(input)
+  letter_array = input.chars
+  unique_letters = letter_array.uniq
+  if unique_letters.length < letter_array.length
+    false
+  else
+    true
+  end
+end
+
+def valid_input?(input)
+  if three_letters?(input) && distinct_letters?(input)
+    true
+  else
+    false
   end
 end
