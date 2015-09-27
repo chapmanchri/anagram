b6f35a5030c066211f30a7197747a9e57555153c
Chris Chapman <chapmanchri@gmail.com>
2015-09-26 22:36:32 -0400

    Add a show word page with links from the main word page and add bootstrap

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,3 +2,13 @@ get '/words' do
   @words = Word.all
   erb :"/words/index"
 end
+
+get '/words/:id' do
+  id = params[:id]
+  puts "***************************"
+  puts id
+  @word = Word.find(id)
+  puts @word.text
+  puts "***************************"
+  erb :"words/show"
+end
